df3b6976e3dab9645ca2baddb5c3e94d2d3c01da
sshhhGit <seungho0497@hanmail.net>
2021-09-18 23:44:28 +0900

    Initial commit

---
+++ src/main/java/com/sshhh/mymvcproject/MyMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sshhh.mymvcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMvcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMvcProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sshhh/mymvcproject/MyMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sshhh.mymvcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMvcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
